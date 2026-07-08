ad498603dd87f5bf8ed3edf9dab5b272c1d16f6c
Nazik <nazikfrontenddeveloper@gmail.com>
2026-07-08 13:54:00 +0300

    add

--- js/gallery.js
+++ js/gallery.js
@@ -1,19 +1,48 @@
 
-const mainImage = document.querySelector('.product-gallery__main-img');
-const thumbnails = document.querySelectorAll('.product-gallery__thumb');
+// const mainImage = document.querySelector('.product-gallery__main-img');
+// const thumbnails = document.querySelectorAll('.product-gallery__thumb');
 
-thumbnails.forEach(thumbnail => {
-  thumbnail.addEventListener('click', () => {
-    const { large, large2x } = thumbnail.dataset;
+// thumbnails.forEach(thumbnail => {
+//   thumbnail.addEventListener('click', () => {
+//     const { large, large2x } = thumbnail.dataset;
 
-    mainImage.src = large;
+//     mainImage.src = large;
+//     mainImage.srcset = `${large} 1x, ${large2x} 2x`;
+//     mainImage.alt = thumbnail.alt;
+
+//     thumbnails.forEach(item => {
+//       item.classList.remove('product-gallery__thumb--active');
+//     });
+
+//     thumbnail.classList.add('product-gallery__thumb--active');
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+const mainImage = document.querySelector(".product-gallery__main-img");
+const thumbnails = document.querySelectorAll(".product-gallery__thumb");
+
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", () => {
+    const large = thumbnail.dataset.large;
+    const large2x = thumbnail.dataset.large2x;
+
     mainImage.srcset = `${large} 1x, ${large2x} 2x`;
+    mainImage.src = large;
     mainImage.alt = thumbnail.alt;
 
-    thumbnails.forEach(item => {
-      item.classList.remove('product-gallery__thumb--active');
+    thumbnails.forEach((item) => {
+      item.classList.remove("product-gallery__thumb--active");
     });
 
-    thumbnail.classList.add('product-gallery__thumb--active');
+    thumbnail.classList.add("product-gallery__thumb--active");
   });
 });
